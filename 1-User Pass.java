832b5ce689e1114ff646e6697d274a4b1578b3c1
louisse26 <44987591+louisse26@users.noreply.github.com>
2018-11-16 08:33:00 +0800

    Add files via upload

---
+++ 1-User Pass.java
@@ -0,0 +1,32 @@
+package account;
+import java.util.*;
+public class Account {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Username: ");
+        String name = sc.nextLine();
+        
+        System.out.println("Password: ");
+        String pw = sc.nextLine();
+        
+        System.out.println("Verification Password: ");
+        String vp = sc.nextLine();
+        
+            if (pw.equals (vp)){
+                System.out.println("Correct Password");
+            
+            }else{
+                System.out.println("Invalid Password");
+            
+                    
+        }
+          
+        
+        
+        
+
+    }
+    
+}
